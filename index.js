06a5d709b154b7ae0c9c8ba966c67032981fa503
dave <dave@lead.davyrice.com>
2015-04-18 11:48:06 -0700

    Fix the transcode routine for ffmpeg.  added static dir for outputted clips.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ var config = require("config");
 var bodyParser = require("body-parser");
 var app = express();
 
+app.use(express.static(__dirname + "/clips"));
 app.use(bodyParser.json());
 
 var mythtv = require("./mythFrontendClient").create(config.mythfrontend.hostname);
@@ -14,8 +15,30 @@ app.post("/mythtv/clip", function(req, res) {
 	var duration = req.body.duration;
 	mythtv.getStatus(function(err, frontendStatus) {
 		if(err==null){
+			var offset = frontendStatus.playedtime;
 			var pathname = path.basename(frontendStatus.pathname)
-			res.status(200).json({"status":"ok","duration":duration, "pathname": pathname});
+			var starttime = frontendStatus.starttime;
+			var title = frontendStatus.title;
+			if(duration && offset && pathname && starttime && title) {
+				transcodeJobManager.startJob({
+					"status":"ok",
+					"duration":duration,
+					"offset": offset,
+					"pathname": pathname,
+					"starttime": starttime,
+					"title": title
+				}, function(err,result) {
+					if(err==null) {
+						res.status(200).json(result);
+					}
+					else {
+						res.status(400).json({"error":"job not done"});
+					}
+				});
+			}
+			else {
+				res.status(400).end();
+			}
 		}
 		else {
 			res.status(400).end();
